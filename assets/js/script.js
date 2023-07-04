00043c4580c25555521cae12afae655f568bce13
Urvishsakhareliya <106795173+Urvishsakhareliya@users.noreply.github.com>
2023-07-04 11:46:50 +0530

    04/07/2023

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 $(".imageItem").click(function () {
+  $(this).addClass("active").siblings().removeClass("active");
   var src = $(this).find("img").attr("src");
 
   $(".coverImage").attr("src", src);
 });
+// git add .;git commit -m '29/06';git push -u origin master;
